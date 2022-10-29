93543809d1997103a3c4ed03a233c889b0480b05
Vira Murray <vira.avramenko@gmail.com>
2022-10-29 13:22:24 -0400

    Add project files.

---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        //to make the "Id" columnthe primary key use data annotation/attribute [Key]
+        public int Id { get; set; }
+        //to make sure that "Name" is a required property use [Required]
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreateDateTime { get; set; } = DateTime.Now;
+    }
+}
